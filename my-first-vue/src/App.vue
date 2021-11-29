0cd6afaf00a2e0f99420c3ea922eea7364257077
austiniqer <63011075@kmitl.ac.th>
2021-11-29 22:28:43 +0700

    v-bind,v-on

--- my-first-vue/src/App.vue
+++ my-first-vue/src/App.vue
@@ -1,94 +1,90 @@
 <template>
-  <div class="contain">
-    <div class="wrap">
-      <img :src="img_src" alt="" class="img_avt" />
-      <button @click="toggleVisible">Detail</button>
-      <!-- v-show แสดงหรือซ่อน ก็คือ style="none"  -->
-      <article v-show="isVisible" class="article"> 
-        <h3>{{ getFullName }}</h3>
-        <h3>Age : {{ age }}</h3>
-        <p>Address : {{ address }}</p>
-        <p>Contact : <a :href="github" target="_blank">Github</a></p>
-        <p v-if="hobby.length === 0">No hobby</p>
-        <div v-else>
-          <p>Hobby</p>
-          <ul>
-            <!-- for loop -->
-            <li v-for="(item, idx) in hobby" :key="idx">{{ item }}</li>
-          </ul>
-        </div>
-      </article>
-      <!-- if else  -->
-    </div>
+  <div class="container">
+    <Header title="Hello" />
+    <Tasks :tasks="tasks" />
   </div>
 </template>
 
 <script>
+import Header from "./components/Header.vue";
+import Tasks from './components/Tasks.vue'
+
 export default {
   name: "App",
-  data() {
+  components: {
+    Header,
+    Tasks,
+  },
+  data(){
     return {
-      firstname: "austiniqer",
-      age: 19,
-      address: "Lardkrabang, Bangkok",
-      img_src: "https://avatars.githubusercontent.com/u/69495813?v=4",
-      github: "https://github.com/Abdulhakim-Maha",
-      isVisible: false,
-      hobby: ["programming", "game", "watching youtube", "sleeping"],
-    };
+      tasks: []
+    }
   },
-  methods: {
-    toggleVisible() {
-      this.isVisible = !this.isVisible;
-    },
-  },
-  computed: {
-   getFullName(){
-     return `${this.firstname} maha`
-   } 
-  },
+  created(){
+    this.tasks = [
+      {
+        id:1,
+        text: 'Doctor Appointment',
+        day : ' 1/12/2564',
+        reminder: true,
+      },
+      {
+        id:2,
+        text: 'Linear algebra Final',
+        day : ' 3/12/2564',
+        reminder: true,
+      },
+      {
+        id:3,
+        text: 'Final Project',
+        day : ' 17/12/2564',
+        reminder: false,
+      }
+    ]
+  }
 };
 </script>
 
 <style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap");
 * {
-  padding: 0;
-  margin: 0 auto;
   box-sizing: border-box;
+  margin: 0;
+  padding: 0;
 }
-.contain {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-content: center;
-  width: 100vw;
-  height: 100vh;
-  /* background-color: bisque; */
+body {
+  font-family: "Poppins", sans-serif;
 }
-.wrap {
-  width: 50%;;
-  height: 50%;
-  display: flex;
-  flex-direction: column;
-  padding: 3rem;
-  border-radius: 10px;
-  border : 1px solid gray;
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
 }
-button{
-  margin-top: 5px;
-  background-color: bisque;
-  width: 100px;
-  height: 50px;
-  border-radius: 10px;
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
   cursor: pointer;
-}
-.img_avt {
-  width: 90px;
-  height: 90px;
-  border-radius: 50%;
-}
-a {
   text-decoration: none;
-  color: blue;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
 }
 </style>
---
+++ my-first-vue/src/components/Tasks.vue
@@ -0,0 +1,18 @@
+<template>
+  <div :key="task" v-for="task in tasks">
+    <Task :task="task" />
+  </div>
+</template>
+
+<script>
+import Task from "./Task.vue";
+export default {
+  name: "Tasks",
+  components: {
+    Task,
+  },
+  props: {
+    tasks: [],
+  },
+};
+</script>
