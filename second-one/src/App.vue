01af024ab5fb9564b4e4576df903390d3a10eff9
austiniqer <63011075@kmitl.ac.th>
2021-12-02 04:10:10 +0700

    Calendar

---
+++ second-one/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <AppHeader />
+  <div class="w-full flex">
+    <Calendar />
+  </div>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+import Calendar from "./components/Carlendar.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+    Calendar,
+  },
+};
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+</style>
---
+++ second-one/src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav
+    class="w-full bg-gradient-to-r from-blue-800 to-blue-200 text-white px-4 py-2"
+  >
+    hello
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style></style>
